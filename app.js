daeedd7ac334b5227b3f818bff536d5e7087c662
svkutyrev <52482757+svkutyrev@users.noreply.github.com>
2019-11-19 23:16:16 +0300

    update add files

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
