9598c68c781d14e23f04cf91f53150118ec6b4c2
svkutyrev <52482757+svkutyrev@users.noreply.github.com>
2019-11-19 19:00:00 +0400

    update file app

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-const http = require('http');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const fs = require('fs');
-const path = require('path');
 const router = require('./routes/users');
 const cards = require('./routes/cards');
 const pages = require('./routes/pages');
@@ -14,7 +11,6 @@ const pages = require('./routes/pages');
 const { PORT = 3000 } = process.env;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
