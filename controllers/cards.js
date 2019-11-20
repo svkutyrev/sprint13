c1c10a27770a20fb5659a3e849fc23e05cba6882
svkutyrev <52482757+svkutyrev@users.noreply.github.com>
2019-11-20 19:55:53 +0400

    update files

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 
 const Card = require('../models/card');
 
@@ -9,27 +8,27 @@ module.exports.createCard = (req, res) => {
   const owner = req.user._id;
   Card.create({ name, link, owner })
     .then(card => res.send({ card }))
-    .catch(err => res.status(404).send({ message: err.message }))
+    .catch(err => res.status(404).send({ message: err.message }));
 };
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-  .populate('card')
-  .then(cards => res.send({ cards }))
-  .catch(err => res.status(404).send({ message: err.message }));
+    .populate('card')
+    .then(cards => res.send({ cards }))
+    .catch(err => res.status(404).send({ message: err.message }));
 
 };
 
 module.exports.deleteCard = (req, res ) => {
   Card.findByIdAndRemove(req.params.cardId)
-  .then(card => {
-    if (!card){
-      res.status(404).send({ message: 'Карточка не найдена' })
-    }
-    else{
-      res.send({ data: card })
-    }
-  })
-  .catch(err => res.status(404).send({ message: err.message }));
+    .then(card => {
+      if (!card) {
+        res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      else{
+        res.send({ data: card })
+      }
+    })
+    .catch(err => res.status(404).send({ message: err.message }));
 };
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+
 const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
@@ -11,21 +13,21 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar} = req.body;
   User.create({ name, about, avatar})
     .then(user => res.send({ user }))
-    .catch(err => res.status(404).send({ message: err.message }))
+    .catch(err => res.status(404).send({ message: err.message }));
 };
 
-module.exports.getUser = (req, res, err) => {
+module.exports.getUser = (req, res) => {
   const {_id} = req.user;
   User.findById(req.params.id)
-  .then(user => {
-    if(!user){
-      return res.status(404).send({message: 'Такой пользователь не найден'})
-    }
-    else{
-      res.send({ user })
-    }
-  })
-  .catch(err => res.status(404).send({ message: 'юзер не найден' }));
+    .then(user => {
+      if(!user) {
+        return res.status(404).send({message: 'Такой пользователь не найден'});
+      }
+      else{
+        res.send({ user })
+      }
+    })
+    .catch(err => res.status(404).send({ message: 'юзер не найден' }));
 
 }
 
